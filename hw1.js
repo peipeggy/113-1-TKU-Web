a26598e9f5f91711564ce1abc64d5a9704944d73
peipeggy <164894958+peipeggy@users.noreply.github.com>
2024-10-24 11:25:03 +0800

    1024

---
+++ hw1.js
@@ -0,0 +1,5 @@
+var num = 25;
+var n = "Alice";
+n = "Bob";
+console.log("Age: ".concat(num));
+console.log("Name: ".concat(n));
---
+++ hw1.ts
@@ -0,0 +1,5 @@
+const num:number = 25;
+let n: string = "Alice";
+n = "Bob";
+console.log(`Age: ${num}`);
+console.log(`Name: ${n}`);
